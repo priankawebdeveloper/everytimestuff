857643f1d4041f731b4e26b2feb90356e5ba984e
priankawebdeveloper <priankawebdeveloper@gmail.com>
2017-09-10 22:32:20 +0530

    make it better

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice', imageurl: '/assets/images/k2.jpg' },
+  { id: 12, name: 'Mr. Nice', imageurl: '/assets/images/k2.jpg' },
+  { id: 13, name: 'Mr. Nice', imageurl: '/assets/images/lighter.jpg' },
+  { id: 14, name: 'Mr. Nice', imageurl: '/assets/images/pin.jpg' },
+  { id: 15, name: 'Mr. Nice', imageurl: '/assets/images/socks.jpg' },
+  { id: 16, name: 'Mr. Nice', imageurl: '/assets/images/blanket.jpg' },
+  { id: 17, name: 'Mr. Nice', imageurl: '/assets/images/tacoHolder.jpg' },
+  { id: 18, name: 'Mr. Nice', imageurl: '/assets/images/pin.jpg' },
+ 
+];
+
